98a8337489d2cc1a4a6ecf76d4fd625bab530659
benjamin <benjamin@kakao.com>
2016-04-20 12:25:54 +0900

    capistrano 3.4.1 locking. 릴리즈 폴더 30개 유지하도록.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,6 @@
+# config valid only for current version of Capistrano
+lock '3.4.1'
+
 set :application, 'lovehouse'
 set :repo_url, 'git@github.com:BenjaminKim/lovehouse.git'
 set :branch, 'master'
@@ -18,7 +21,7 @@ set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :default_env, { rvm_bin_path: '~/.rvm/bin' }
-set :keep_releases, 5
+set :keep_releases, 30
 
 #set :unicorn_rack_env, :production
 
