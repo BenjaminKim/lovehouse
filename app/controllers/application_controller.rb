dff0526f6d1ded86db280b370d52151fdad4be24
benjamin <benjamin@kakao.com>
2016-07-12 23:22:23 +0900

    redis 아답터 셋팅. 페이지뷰 볼수 있도록 추가.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  after_action :report_page_view
+
+  private
+    def report_page_views
+      key = "pv:#{Date.today}"
+      REDIS_POOL.with do |conn|
+        conn.hincrby(key, controller_name, 1)
+      end
+    end
 end
