97e6d3564f086fba4b12f0ba1690093034933cf1
Benjamin <jehokim@live.com>
2017-01-18 14:01:13 +0900

    타이틀 처리방식 변

--- app/controllers/one_room_controller.rb
+++ app/controllers/one_room_controller.rb
@@ -1,6 +1,7 @@
 class OneRoomController < ApplicationController
   def index
     set_meta_tags(
+      title: '원룸 사진들'.freeze,
       og: {
         title: '원룸 사진'.freeze,
         image: helpers.image_url('one/원룸-전경.png'.freeze),
--- app/controllers/two_room_controller.rb
+++ app/controllers/two_room_controller.rb
@@ -1,6 +1,7 @@
 class TwoRoomController < ApplicationController
   def index
     set_meta_tags(
+      title: '투룸 사진들'.freeze,
       og: {
         title: '투룸 사진'.freeze,
         image: helpers.image_url('two/301호-거실2.jpg'.freeze),
